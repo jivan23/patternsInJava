0ebe327fce28b23a9aea5695957c4b35806577d0
jivan23 <128153095+jivan23@users.noreply.github.com>
2024-02-15 11:11:29 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,55 @@
+
+
+//class Pattern
+// {
+//     public static void main(String[] args) 
+//     {
+//         int n = 5;
+//         int nSt =1;
+//         int nSp = 2;
+
+
+//         for(int i=0;i<=n;i++)
+//         {
+//             for(int j=1;j<=nSp;j++)
+//             {
+//                 System.out.print(" ");
+//             }
+            
+//             for(int k=1;k<=nSt;k++)
+//             {
+//                 System.out.print("*");
+//             }
+
+//             if(i<n/2)
+//             {
+//                 nSp--;
+//                 nSt  = nSt + 2;
+//             }
+//             else
+//             {
+//                 nSp++;
+//                 nSt= nSt-2;
+//             }
+
+//             System.out.println();
+//         }
+        
+//     }
+// }
+
+ 
+//   * 
+//  ***
+// *****
+//  *** 
+//   *  
+
+
+class Pattern
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
